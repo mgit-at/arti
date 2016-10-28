061e8eecec948acce244f2af18c6d9b82505bf65
Christian Pointner <cpointner@mgit.at>
2016-10-28 15:18:33 +0200

    initial structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# arti
+
+Upload tool for build artifacts and sourcecode tarballs to S3 and similar
+cloud storage.
+This is more general approach to what [[https://github.com/krobertson/deb-s3]]
+does.
+
+...tba...
---
+++ cmd/root.go
@@ -0,0 +1,72 @@
+// Copyright © 2016 mgIT GmbH <office@mgit.at>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "arti",
+	Short: "Push artifacts to S3 compatible storeage",
+	Long: `This tool can be used to push build artifacts or source code
+tarballs to a cloud storage provider such as Amazon S3.
+
+arti will create a directory structure based on semantic
+versioning. It allows to upload any tarball/build-artifact
+and can also be used to list all uploaded files.`,
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.arti.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".arti") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+	viper.AutomaticEnv()           // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
