0a3a4152e4bb849cf4dcebfa12f60249c56f359a
Christian Pointner <cpointner@mgit.at>
2016-10-28 15:23:03 +0200

    fixed copyright header

--- cmd/ls.go
+++ cmd/ls.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2016 mgIT GmbH <office@mgit.at>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
